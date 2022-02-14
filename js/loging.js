0fe6f6d1684c3879ea25baf70f7800a33e6db7a3
MDMEHEDIHERO <mehedihassan05861>
2022-02-15 00:07:09 +0600

    finished commit

---
+++ js/loging.js
@@ -0,0 +1,17 @@
+document.getElementById('loging-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get userPassword 
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html'
+    }
+});
+
+
+
+
+
+
